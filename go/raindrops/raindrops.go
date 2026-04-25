74e2cd8dde5c15518991a78794737074b9a939f2
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2026-04-25 14:41:00 +0200

    completed go basic exercise. detail: https://exercism.org/tracks/go/exercises/raindrops

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,21 @@
+package raindrops
+
+import "strconv"
+
+func Convert(number int) string {
+	sounds := ""
+    if number % 3 == 0 {
+        sounds += "Pling"
+    }
+    if number % 5 == 0 {
+        sounds += "Plang"
+    }
+    if number % 7 == 0 {
+        sounds += "Plong"
+    }
+
+    if number % 3 != 0 && number % 5 != 0 && number % 7 != 0 {
+        return strconv.Itoa(number)
+    }
+    return sounds
+}
